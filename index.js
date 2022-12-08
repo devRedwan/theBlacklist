3dbc6920cf86b70a745c4209f4d1fb0cb61805b3
Redwan Ahmed <ahmed.redwan7331@gmail.com>
2022-12-07 20:44:28 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+to add filters use '?' after url to add and '&' to connect them
+- to filter using limit, use '$limit=[int]'
+- to filter using columns use 'columnName = columnValue'
+- Total Pay = total_ot_paid + total_other_pay + regular_gross_paid
+For more filtering tutorial: https://dev.socrata.com/docs/endpoints.html
+*/
+
+async function cityPayrollapi() {
+  const fetchAPI = await fetch(
+    "https://data.cityofnewyork.us/resource/k397-673e.json?$limit=500000&agency_name=POLICE%20DEPARTMENT&$select=*"
+  );
+  const fetchData = await fetchAPI.json();
+  console.log(fetchData);
+}
+
+async function brooklynReportedFelonies() {
+  const fetchAPI = await fetch(
+    "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=600000&boro_nm=QUEENS&law_cat_cd=FELONY"
+  );
+  const fetchData = await fetchAPI.json();
+  console.log("felonies", fetchData);
+}
