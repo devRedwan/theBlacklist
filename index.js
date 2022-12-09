7a5ad70c6395c0590ba841968f36c18d1e015a98
Redwan Ahmed <ahmed.redwan7331@gmail.com>
2022-12-08 22:58:36 -0500

    Just running around with Bootstrap

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ to add filters use '?' after url to add and '&' to connect them
 - to filter using columns use 'columnName = columnValue'
 - Total Pay = total_ot_paid + total_other_pay + regular_gross_paid
 For more filtering tutorial: https://dev.socrata.com/docs/endpoints.html
+5uac-w243 - NYPD Year-To-Date Compaint History unique identifier
+sort by cmplnt_fr_dt
 */
 
-async function cityPayrollapi() {
-  const fetchAPI = await fetch(
-    "https://data.cityofnewyork.us/resource/k397-673e.json?$limit=500000&agency_name=POLICE%20DEPARTMENT&$select=*"
-  );
-  const fetchData = await fetchAPI.json();
-  console.log(fetchData);
-}
+// async function cityPayrollapi() {
+//   const fetchAPI = await fetch(
+//     "https://data.cityofnewyork.us/resource/k397-673e.json?$limit=500000&agency_name=POLICE%20DEPARTMENT&$select=*"
+//   );
+//   const fetchData = await fetchAPI.json();
+//   console.log(fetchData);
+// }
 
-async function brooklynReportedFelonies() {
-  const fetchAPI = await fetch(
-    "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=600000&boro_nm=QUEENS&law_cat_cd=FELONY"
-  );
-  const fetchData = await fetchAPI.json();
-  console.log("felonies", fetchData);
-}
+// async function brooklynReportedFelonies() {
+//   const fetchAPI = await fetch(
+//     "https://data.cityofnewyork.us/resource/qgea-i56i.json?$limit=600000&boro_nm=QUEENS&law_cat_cd=FELONY"
+//   );
+//   const fetchData = await fetchAPI.json();
+//   console.log("felonies", fetchData);
+// }
+
+//------------------------------------------------------------------------------------------
+
